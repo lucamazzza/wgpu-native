7ac6c0c1736204b8929cf446358a4c3598f3a83c
Luca Mazza <luca.mazza@mazluc.ch>
2026-06-18 23:02:17 +0200

    hack: added VkImage to WGPU convertor hack

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ use conv::{
     map_query_set_index, map_sampler_border_color_extras, map_shader_module,
     map_shader_runtime_checks, map_surface, map_surface_configuration, CreateSurfaceParams,
 };
+use naga::Handle;
 use parking_lot::Mutex;
 use smallvec::SmallVec;
 use std::{
@@ -24,8 +25,7 @@ use utils::{
     string_view_into_label, string_view_into_str, texture_format_has_depth,
 };
 use wgc::{
-    command::{bundle_ffi, ComputePass, RenderPass},
-    id, resource, Label,
+    Label, command::{ComputePass, RenderPass, bundle_ffi}, device::global, id, resource
 };
 
 use crate::conv::map_primitive_state;
@@ -4875,9 +4875,6 @@ pub unsafe extern "C" fn wgpuRenderPassEncoderWriteTimestamp(
 
 // Vk Handles
 
-
-// Vulkan Natives injection
-
 #[no_mangle]
 pub unsafe extern "C" fn wgpuInstanceGetVulkanInstance(instance: crate::native::WGPUInstance) -> *mut c_void {
     #[cfg(all(any(target_os = "windows", target_os = "linux"), feature = "vulkan"))]
@@ -4928,6 +4925,68 @@ pub unsafe extern "C" fn wgpuDeviceGetVulkanDevice(device: crate::native::WGPUDe
     #[cfg(not(all(any(target_os = "windows", target_os = "linux"), feature = "vulkan")))]
     {
         let _ = device;
+        std::ptr::null_mut()
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn wgpuDeviceCreateTextureFromVulkanImage(
+    device: crate::native::WGPUDevice,
+    vk_image: *mut c_void,
+    descriptor: Option<&native::WGPUTextureDescriptor>,
+) -> native::WGPUTexture {
+    #[cfg(all(any(target_os = "windows", target_os = "linux"), feature = "vulkan"))]
+    {
+        let (device_id, context, error_sink) = {
+            let device = device.as_ref().expect("invalid device");
+            (device.id, &device.context, &device.error_sink)
+        };
+        let descriptor = descriptor.expect("invalid descriptor");
+        let desc = wgt::TextureDescriptor {
+            label: string_view_into_label(descriptor.label),
+            size: conv::map_extent3d(&descriptor.size),
+            mip_level_count: descriptor.mipLevelCount,
+            sample_count: descriptor.sampleCount,
+            dimension: conv::map_texture_dimension(descriptor.dimension).unwrap_or(wgt::TextureDimension::D2),
+            format: conv::map_texture_format(descriptor.format).expect("invalid texture format for texture descriptor"),
+            usage: from_u64_bits(descriptor.usage).expect("invalid texture usage for texture descriptor"),
+            view_formats: make_slice(descriptor.viewFormats, descriptor.viewFormatCount)
+                .iter()
+                .map(|v| { conv::map_texture_format(*v).expect("invalid view format for texture descriptor") })
+                .collect(),
+        };
+        use ash::vk::Handle;
+        let raw_image = ash::vk::Image::from_raw(vk_image as u64);
+        let hal_texture = hal::vulkan::Texture { // NOTE: This is not found on macos => check windows
+            raw: raw_image,
+            drop_guard: None,
+        };
+        let (texture_id, error) = context.create_texture_from_hal::<hal::api::Vulkan>( // NOTE: Also this is not found...
+            hal_texture,
+            &desc,
+            device_id,
+        );
+        if let Some(cause) = error {
+            handle_error(error_sink, cause, desc.label, "wgpuDeviceCreateTexture");
+        }
+        Arc::into_raw(Arc::new(WGPUTextureImpl {
+            context: context.clone(),
+            id: texture_id,
+            error_sink: error_sink.clone(),
+            surface_id: None,
+            has_surface_presented: Arc::default(),
+            data: TextureData {
+                usage: descriptor.usage,
+                dimension: descriptor.dimension,
+                size: descriptor.size,
+                format: descriptor.format,
+                mip_level_count: descriptor.mipLevelCount,
+                sample_count: descriptor.sampleCount,
+            },
+        }))
+    }
+    #[cfg(not(all(any(target_os = "windows", target_os = "linux"), feature = "vulkan")))]
+    {
         std::ptr::null_mut()
     }
 }
